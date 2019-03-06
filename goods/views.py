60ef50861b88757e550cbe1d5ce7c3a1e8427cad
loser <xlm951024@gmail.com>
2019-03-06 21:46:15 +0800

    推荐商品，通过装饰器完成

--- goods/views.py
+++ goods/views.py
@@ -48,11 +48,44 @@ class IndexView(View):
         })
 
 
+# 装饰器
+def recommend_view(func):
+    def wrapper(detailView, request, goodsid, *args, **kwargs):
+        # 将存放在cookie中的goodsid获取
+        cookie_str = request.COOKIES.get('recommend', '')
+
+        # 存放所有goodsid的列表
+        goodsIdList = [gid for gid in cookie_str.split() if gid.strip()]
+
+        # 最终需要获取的推荐商品
+        goodsObjList = [Goods.objects.get(id=gsid) for gsid in goodsIdList if
+                        gsid != goodsid and Goods.objects.get(id=gsid).category_id == Goods.objects.get(
+                            id=goodsid).category_id][:4]
+
+        # 将goodsObjList 传递给get方法
+        response = func(detailView, request, goodsid, goodsObjList, *args, **kwargs)
+
+        # 判断goodsid是否存在idlist中
+        if goodsid in goodsIdList:
+            goodsIdList.remove(goodsid)
+            goodsIdList.insert(0, goodsid)
+        else:
+            goodsIdList.insert(0, goodsid)
+
+        # 将goodsidlist放到cookie中
+        response.set_cookie('recommend', ' '.join(goodsIdList), max_age=3 * 24 * 60)
+
+        return response
+
+    return wrapper
+
+
 class DetailView(View):
-    def get(self, request, goodsId):
+    @recommend_view
+    def get(self, request, goodsId, recommendList=[]):
         goodsId = int(goodsId)
 
         # 根据goodsid 查询商品详情信息(goods对象）
         goods = Goods.objects.get(id=goodsId)
 
-        return render(request, 'netshop/detail.html', {'goods': goods})
+        return render(request, 'netshop/detail.html', {'goods': goods, 'recommendList': recommendList})
